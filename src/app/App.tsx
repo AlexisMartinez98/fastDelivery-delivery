36591d7dd68386043207a258924ff9739ab3699b
FranMack <franmack556@gmail.com>
2023-11-01 18:27:13 -0300

    feat: button link finalizar

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './states/store';
+
+const AppContainer: React.FC = () => {
+  return (
+    
+     <div></div>
+    
+  );
+};
+
+export default AppContainer;

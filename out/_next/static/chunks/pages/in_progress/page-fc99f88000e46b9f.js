f6dee59b9f555f96b6f509c994f2299edaf9da5e
Micaela <micaelauvilla9@gmail.com>
2023-11-15 13:33:51 -0300

    feat: create out in root

---
+++ out/_next/static/chunks/pages/in_progress/page-fc99f88000e46b9f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[802],{6448:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/in_progress/page",function(){return u(5163)}])}},function(n){n.O(0,[774,255,141,664,163,888,179],function(){return n(n.s=6448)}),_N_E=n.O()}]);
